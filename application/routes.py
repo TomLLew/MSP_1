fedb9cc28a373784d89b151977b6476f9bfab1f5
TomLLew <thomas.llewelyn@academytrainee.com>
2020-01-17 11:07:16 +0000

    return hello on home

--- application/routes.py
+++ application/routes.py
@@ -1,2 +1,7 @@
 from flask import render_template, redirect, url_for, request
 from application import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Homepage')
