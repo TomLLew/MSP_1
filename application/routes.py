25400586eb0189d591d58e8d8c138928035f3bd5
TomLLew <thomasllewelyn.tl@gmail.com>
2020-01-23 23:41:31 +0000

    test download image

--- application/routes.py
+++ application/routes.py
@@ -17,6 +17,9 @@ def home():
 @login_required
 def posts():
     postData = Posts.query.all()
+    image = User.query.filter_by(user_id=current_user.id).all()
+    payload = "https://msp-1-bucket-1579257693.s3.amazonaws.com/"+image['image'
+    print (payload)
     return render_template('posts.html', title='Posts', posts=postData)
 
 @app.route('/account')
